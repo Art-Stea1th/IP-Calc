cab50ee14be836b9562e38467a9ed0f288f0e181
Stanislav Kuzmitch <art.stea1th@gmail.com>
2015-07-06 21:35:42 +0300

    print table

--- IP-Calc/Source.cpp
+++ IP-Calc/Source.cpp
@@ -4,40 +4,45 @@
 #include <conio.h>
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-void IPArrAlloc(int** &pArr, int row, int col),
-	 IPArrRelease(int** &pArr, int row);
+void arrAlloc(int** &pArr, int row, int col),
+	 arrRelease(int** &pArr, int row);
 
-void IPArrInit(int** &pArr, int row, int col),
-	 IPArrPrint(int** &pArr, int row, int col);
+void arrInit(int** &pArr, int row, int col),
+	 arrPrint(int** &pArr, int row, int col);
+
+void arrPrintDescription(int n);
 
 int GetInt();
 void CinWait();
 
 // Entry point
 int main(){
-	int **IPArr = NULL;
-	int col(4), row(7);
+	int **resultArr = nullptr,
+		col(4), row(7);
+	int bit(0);
 
-	IPArrAlloc(IPArr, row, col);
-	IPArrInit(IPArr, row, col);
-	IPArrPrint(IPArr, row, col);
-	IPArrRelease(IPArr, row);	
+	arrAlloc(resultArr, row, col);
+	arrInit(resultArr, row, col);
+	arrPrint(resultArr, row, col);
 
+	
+	arrRelease(resultArr, row);
 	CinWait();
 	return 0;
 }
 
 // Allocates memory for an array
-void IPArrAlloc(int** &pArr, int row, int col){
+void arrAlloc(int** &pArr, int row, int col){
 	pArr = new int*[row];
 
 	for (int i(0); i < row; i++)
 		pArr[i] = new int[col];
 }
 // Release memory for an array
-void IPArrRelease(int** &pArr, int row){
+void arrRelease(int** &pArr, int row){
 	
 	for (int i(0); i < row; i++)
 		delete[] pArr[i];
@@ -45,22 +50,44 @@ void IPArrRelease(int** &pArr, int row){
 	delete[] pArr;
 }
 
-// Init Array
-void IPArrInit(int** &pArr, int row, int col){
+// Init Array (null)
+void arrInit(int** &pArr, int row, int col){
 
 	for (int i(0); i < row; i++)
 		for (int j(0); j < col; j++)
 			pArr[i][j] = 0;
 }
 // Print Array
-void IPArrPrint(int** &pArr, int row, int col){
+void arrPrint(int** &pArr, int row, int col){
+	cout << "\n   IPv4 Calculator by Art.Stea1th (Stanislav Kuzmitch)\n\n";
 
+	for (int i(0); i < 80; i++) cout << '-';
+	
+	cout << "\n";
 	for (int i(0); i < row; i++){
+		arrPrintDescription(i);
 		for (int j(0); j < col; j++){
-			cout << pArr[i][j];
+			cout << setw(3) << pArr[i][j];
 			if (j != col - 1) cout << '.';
 		}
-		cout << '\n';
+		if (i == 1) cout << " | " << 32 << " bit\n";
+		else if (i == 2) cout << "\n\n";
+		else cout << '\n';
+	}
+	cout << '\n' << setw(16) << "Hosts: " << setw(15) << 256;
+}
+
+// Print Description Array Strings
+void arrPrintDescription(int n){
+	switch (n){
+	case 0: cout << setw(16) << "Adress: "; break;
+	case 1: cout << setw(16) << "Netmask: "; break;
+	case 2: cout << setw(16) << "Wildcard: "; break;
+	case 3: cout << setw(16) << "Network: "; break;
+	case 4: cout << setw(16) << "Host Min: "; break;
+	case 5: cout << setw(16) << "Host Max: "; break;
+	case 6: cout << setw(16) << "Broadcast: "; break;
+	default: cout << setw(16); break;
 	}
 }
 
