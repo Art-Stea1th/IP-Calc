dd68935b5ce4c6fa9acca1f4bf264c2935bce10b
Stanislav Kuzmich <art.stea1th@gmail.com>
2016-02-13 03:23:57 +0300

    Rename resource.h to Resources/Resource.h

---
+++ Resources/Resource.h
@@ -0,0 +1,35 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_MAIN                        100
+#define IDI_IP_CALC                     101
+#define IDC_GROUP_NETWORK_PARAM         200
+#define IDC_STATIC_TEXT_IP_ADDRESS      301
+#define IDC_STATIC_TEXT_NETW_MASK       302
+#define IDC_STATIC_TEXT_WILDCARD        303
+#define IDC_STATIC_TEXT_NETWORK         304
+#define IDC_STATIC_TEXT_HOSTMIN         305
+#define IDC_STATIC_TEXT_HOSTMAX         306
+#define IDC_STATIC_TEXT_BROADCAST       307
+#define IDC_IP_IP_ADDRESS               401
+#define IDC_IP_NETW_MASK                402
+#define IDC_IP_WILDCARD                 403
+#define IDC_IP_NETWORK                  404
+#define IDC_IP_HOSTMIN                  405
+#define IDC_IP_HOSTMAX                  406
+#define IDC_IP_BROADCAST                407
+#define IDC_IP_IP_ADDRESS_IN            501
+#define IDC_COMBO_BITMASK               502
+#define IDC_BTN_CALCULATE               503
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
